dcfafb69de143319677745ff9070f954a9fa0953
yinsong1205 <yinsong@1205.com>
2017-10-12 15:43:56 +0800

    报关单完成

---
+++ WebApp/static/js/guandan/guandan_list.js
@@ -0,0 +1,151 @@
+$(function() {
+	var $table = $('#table');
+	/*
+	 * 显示行号 { formatter : function(value,row,index){ return index+1; } }
+	 */
+	var columns = [ {
+		field : 'state',
+		checkbox : true
+	}, {
+		title : '关单号',
+		field : 'id',
+		formatter : formatterLookInfo
+	}, {
+		title : '出口日期',
+		field : 'ckDate'
+	}, {
+		title : '贸易方式',
+		field : 'myType'
+	}, {
+		title : '出口口岸',
+		field : 'kouan'
+	}, {
+		title : '出口国别',
+		field : 'guobie'
+	}, {
+		title : '申报状态',
+		field : 'shstate',
+		align:'center',
+		formatter : formatterStatus
+	}, {
+		title : '任务编号',
+		field : 'rwbh'
+	}
+	];
+	var data = [ {
+		"id" : "000000000092371271",
+		"ckDate" : "2014-10-19",
+		"myType" : "一般贸易",
+		"kouan" : "天津港",
+		"guobie" : "德国",
+		"rwbh" : "201703120001",
+		"shstate" : "0"
+	}, {
+		"id" : "000000000092371949",
+		"ckDate" : "2017-02-10",
+		"myType" : "一般贸易",
+		"kouan" : "天津港",
+		"guobie" : "日本",
+		"rwbh" : "201703120012",
+		"shstate" : "0"
+	} ];
+
+	$table.bootstrapTable('destroy').bootstrapTable({
+		classes : 'table table-hover table-no-bordered', // 添加样式名称
+		striped : true, // 隔行变色
+		search : true, // 显示搜索工具条
+		searchAlign : 'left',
+		toolbar : '.myBtn',
+		toolbarAlign : 'right',
+		// buttonsAlign : 'left',
+		showExport : true,
+		exportTypes : [ 'txt', 'csv', 'excel' ],
+		pageNumber : 1,
+		pageSize : 10,
+		pageList : [ 10, 20, 50 ],
+		height : $('body').height() - 32,
+		pagination : true,
+		columns : columns,
+		data : data
+	});
+
+	// 查看详情
+	$(document).on("click", ".lookInfo", function() {
+		
+		parent.layer.open({
+			type : 2,
+			skin : 'myLayui', // 样式类名
+			title : "报关单详情",
+			area : [ "80%", "80%" ],
+			content : "../../pages/guandan/guandan/detail.html"
+		});
+	});
+
+	// 添加
+	$(document).on("click", "#add", function() {
+		parent.layer.open({
+			type : 2,
+			skin : 'myLayui', // 样式类名
+			title : "添加出口合同",
+			area : [ "70%", "65%" ],
+			content : "../../pages/hetong/chukou/add.html"
+		});
+	});
+
+	// 修改
+	$(document).on("click","#update",function() {
+		var selectRows = $table.bootstrapTable('getSelections');
+		if (selectRows.length === 1) {
+			parent.layer.open({
+				type : 2,
+				skin : 'myLayui', // 样式类名
+				title : "修改供货商信息",
+				area : [ "70%", "65%" ],
+				content : "../../pages/hetong/chukou/add.html",
+				success : function(layero, index) {
+					var currentFrame = layero.find("iframe")[0].contentWindow.document;
+					setFormData('addInfoForm',currentFrame, selectRows[0]);
+				}
+			});
+		} else {
+			parent.layer.msg("请选择一条数据进行编辑!");
+		}
+	});
+
+	// 删除
+	$(document).on('click', '#delete', function() {
+		var selectRows = $table.bootstrapTable('getSelections');
+		if (selectRows.length >= 1) {
+			parent.layer.confirm('确定要删除吗?', {
+				btn : [ "确定" ]
+			}, function() {
+				selectRows = selectRows.map(function(item) {
+					return item.id;
+				});
+				$table.bootstrapTable('remove', {
+					field : 'id',
+					values : selectRows
+				});
+				parent.layer.msg('删除成功!');
+			});
+		}else{
+			parent.layer.msg("请选择一条数据进行编辑!");
+		}
+	});
+
+	// 查看报关行预关单
+	$(document).on('click', '.lookInfos', function() {
+		parent.layer.open({
+			type : 2,
+			skin : 'myLayui', // 样式类名
+			title : "报关行预关单",
+			area : [ "70%", "65%" ],
+			content : "../../pages/guandan/yuluru/xiangqing.html"
+		});
+
+	});
+
+});
+function formatterLookInfos(value, row, index) {
+	return '<a href="javascript:;" class="text-info lookInfos">' + value + '</a>';
+}
---
+++ WebApp/static/js/yuluru/yuluru_detail.js
@@ -0,0 +1,105 @@
+$(function() {
+
+	// 任务详情信息
+	var infos = [ {
+		name : "预录入编号",
+		value : "000000000092371271"
+	}, {
+		name : "海关编号",
+		value : "HT181898891984"
+	}, {
+		name : "出口口岸",
+		value : "天津新港"
+	}, {
+		name : "备案号",
+		value : ""
+	}, {
+		name : "出口日期",
+		value : "2014-10-19"
+	}, {
+		name : "申报日期",
+		value : "2015-01-29"
+	}, {
+		name : "经营单位:",
+		value : "某集团工程有限公司"
+	}, {
+		name : "运输方式",
+		value : "水路运输"
+	}, {
+		name : "运输工具",
+		value : "轮船"
+	}, {
+		name : "提运单号",
+		value : ""
+	}, {
+		name : "发货单位",
+		value : "某集团工程有限公司"
+	}, {
+		name : "贸易方式",
+		value : "一般贸易"
+	}, {
+		name : "征免性质",
+		value : ""
+	}, {
+		name : "提运单号",
+		value : ""
+	}, {
+		name : "许可证号",
+		value : ""
+	}, {
+		name : "运抵国",
+		value : "德国"
+	}, {
+		name : "指运港",
+		value : "波士顿"
+	}, {
+		name : "境内货源地",
+		value : "PZX63D832"
+	}, {
+		name : "成交方式",
+		value : "FOB"
+	}, {
+		name : "合同协议号",
+		value : "4520000254"
+	}, {
+		name : "件数",
+		value : "32"
+	}, {
+		name : "包装种类",
+		value : ""
+	}, {
+		name : "毛重（kg）",
+		value : ""
+	}, {
+		name : "净重（kg）",
+		value : ""
+	}, {
+		name : "运费",
+		value : ""
+	}, {
+		name : "保费",
+		value : ""
+	}, { 
+		name : "杂费",
+		value : ""
+	}, {
+		name : "集装箱号",
+		value : ""
+	}, {
+		name : "随附单据",
+		value : ""
+	}, {
+		name : "生产厂家",
+		value : ""
+	}, {
+		name : "标记码及备注",
+		value : ""
+	}
+	];
+
+	//任务详情添加
+	initDetailInfo({
+		id : "info",
+		elems : infos
+	});
+});
